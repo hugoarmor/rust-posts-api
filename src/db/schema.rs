68ce249b8e3e328e089f946bae3eaa8ff5645b3d
hugomoreira <hugo.moreira@pointr.co>
2023-11-14 15:24:00 -0300

    add: authors table

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,6 +1,20 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    authors (id) {
+        id -> Int4,
+        #[max_length = 255]
+        email -> Varchar,
+        #[max_length = 255]
+        name -> Varchar,
+        #[max_length = 255]
+        token -> Varchar,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::table! {
     posts (id) {
         id -> Int4,
         title -> Varchar,
@@ -8,5 +22,13 @@ diesel::table! {
         published_at -> Nullable<Timestamp>,
         created_at -> Timestamp,
         updated_at -> Timestamp,
+        author_id -> Nullable<Int4>,
     }
 }
+
+diesel::joinable!(posts -> authors (author_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    authors,
+    posts,
+);
