32dc6f9c4748c8a6b2ff1a742ecb295085673c43
hugomoreira <hugo.moreira@pointr.co>
2023-11-17 12:14:15 -0300

    add: token config handler

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,12 @@
 extern crate clap;
 
+use std::{
+    fs::{DirBuilder, File},
+    io::{Read, Write},
+};
+
 use clap::*;
-use serde::{Serialize, Deserialize};
+use models::post::NewPost;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -14,19 +19,15 @@ struct Cli {
 enum PostCommand {
     Get { id: u32 },
     GetAll,
-    Create(CreatePostArgs),
-}
-
-#[derive(Serialize, Deserialize, Debug, Args)]
-struct CreatePostArgs {
-    title: String,
-    body: String,
+    Create(NewPost),
 }
 
 #[derive(Subcommand, Debug)]
 enum Command {
     #[command(subcommand)]
     Post(PostCommand),
+    #[command()]
+    Config { token: String },
 }
 
 async fn handle_post_command(command: &PostCommand) -> Result<(), anyhow::Error> {
@@ -55,18 +56,50 @@ async fn handle_post_command(command: &PostCommand) -> Result<(), anyhow::Error>
             println!("Get all posts");
             let client = reqwest::Client::new();
 
-            let formatted_body = serde_json::to_string(&CreatePostArgs { title: (args.title.to_string()), body: (args.body.to_string()) })?;
+            let auth_token = get_token()?;
 
-            client.post("http://localhost:8000/posts")
+            let formatted_body = serde_json::to_string(&NewPost {
+                title: args.title.to_string(),
+                body: args.body.to_string(),
+            })?;
+
+            let response = client
+                .post("http://localhost:8000/posts")
+                .header("Authorization", auth_token)
                 .body(formatted_body)
                 .send()
                 .await?;
 
-            println!("Post created");
+            if !response.status().is_success() {
+                println!("Error: {:#?}", response.text().await?);
+                return Ok(());
+            }
+
+            let response: models::post::Post = serde_json::from_str(
+                &response.text().await?,
+            )?;
+
+            println!("Post created with id: {:#?}", response.id);
         }
     }
 
     Ok(())
+}
+
+fn handle_config_command(token: String) -> Result<(), anyhow::Error> {
+    DirBuilder::new().recursive(true).create("config")?;
+    let mut file = File::create("config/token.txt")?;
+    file.write_all(token.as_bytes())?;
+
+    Ok(())
+}
+
+fn get_token() -> Result<String, anyhow::Error> {
+    let mut file = File::open("config/token.txt").expect("You don't have a token configured.");
+    let mut token = String::new();
+    file.read_to_string(&mut token)?;
+
+    Ok(token)
 }
 
 #[tokio::main]
@@ -75,6 +108,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
     match cli.command {
         Command::Post(command) => handle_post_command(&command).await?,
+        Command::Config { token } => handle_config_command(token)?,
     }
 
     Ok(())
